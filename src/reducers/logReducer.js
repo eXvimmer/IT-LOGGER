c025f7da714c735bc1665fac3017a46efd8159c5
Mustafa Hayati <mustafa.hayati1992@gmail.com>
2020-05-07 13:50:58 +0430

    Create Logs reducer, actions and types.

---
+++ src/reducers/logReducer.js
@@ -0,0 +1,36 @@
+const initialState = {
+  logs: null,
+  current: null,
+  loading: false,
+  error: null,
+};
+import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "../actions/types";
+
+const logReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_LOGS:
+      return {
+        ...state,
+        logs: action.payload,
+        loading: false,
+      };
+
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case LOGS_ERROR:
+      console.error(action.payload);
+      return {
+        ...state,
+        error: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default logReducer;
