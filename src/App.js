24dc3a1f1aa543e675e48704e5c0399353510523
Mustafa Hayati <mustafa.hayati1992@gmail.com>
2020-05-07 13:06:03 +0430

    Setup Redux Store and Provider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, Fragment } from "react";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min";
+import { Provider } from "react-redux";
+import store from "./store";
 import SearchBar from "./components/layout/SearchBar";
 import AddBtn from "./components/layout/AddBtn";
 import Logs from "./components/logs/Logs";
@@ -17,17 +19,19 @@ function App() {
   });
 
   return (
-    <Fragment>
-      <SearchBar />
-      <div className="container">
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <TechListModal />
-        <Logs />
-      </div>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className="container">
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
   );
 }
 
